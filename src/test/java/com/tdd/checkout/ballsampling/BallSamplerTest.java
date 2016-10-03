c17e800e9c0f6a75ca4d9967e6edf1e76bb8c9cc
Debasish Mallick <dmalli@sapient.com>
2016-10-03 17:25:48 +0530

    [debasish][checking instance is available]

---
+++ src/test/java/com/tdd/checkout/ballsampling/BallSamplerTest.java
@@ -0,0 +1,17 @@
+package com.tdd.checkout.ballsampling;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class BallSamplerTest {
+
+    @Test
+    public void checkInstance(){
+        assertNotNull(new BallSampler());
+
+    }
+
+}
